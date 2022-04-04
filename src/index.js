879c6792d6cd3568b3a106e973533c74972c4f7a
im-shubhamsharma <im_shubhamsharma@outlook.com>
2022-04-04 17:00:50 +0530

    react practice mini projects

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const currDate = new Date().toLocaleDateString();
+const currTime = new Date().toLocaleTimeString();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <h1>My Netflix Pick</h1>
+    <p>List of 5 Best Series:</p>
+    <ol>
+      <li>The Witcher</li>
+      <li>Friends</li>
+      <li>Two and half men</li> 
+      <li>Suits</li>
+      <li>Fauda</li>
+    </ol>
+
+    <h1>Hello, My name is Shubham</h1>
+    <p>{`Todays date is ${currDate}`}</p>
+    <p>{`Current time is ${currTime}`}</p>
+  </>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
